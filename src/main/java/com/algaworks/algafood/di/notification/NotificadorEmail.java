4242b8f5b02e9f7451a61d1823689568d24d5267
theJoseAlan <salesalan854@gmail.com>
2023-02-09 17:13:52 -0300

    Profiles
    Spring Profiles é uma forma de separar componentes da aplicação que serão disponibilizados em certos ambientes. Uma mesma aplicação pode e comportar de maneiras diferentes em ambientes diferentes

--- src/main/java/com/algaworks/algafood/di/notification/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notification/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notification;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 //Só notifica por e-mail, se pôr em SMS só vai notificar por sms por reconhecer como primário
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Component;
 //@Qualifier("urgente") //Qualifica o componente. ("urgente") é o identificador. Deve ser colocado o mesmo onde houver o @Autowired
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component //Para ser reconhecida como um componente Spring
+@Profile("prod") //Roda na produção. Mas o aplication.proprierties deve ser alterado
 public class NotificadorEmail implements Notificador{
 
+    public NotificadorEmail(){
+        System.out.println("Notificador Email Real");
+    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
