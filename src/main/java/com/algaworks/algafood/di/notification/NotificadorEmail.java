06a67a80bbee348c43ea2ec5abdf26eacab98099
theJoseAlan <salesalan854@gmail.com>
2023-02-10 11:20:13 -0300

    propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notification/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notification/NotificadorEmail.java
@@ -1,21 +1,27 @@
 package com.algaworks.algafood.di.notification;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component //Para ser reconhecida como um componente Spring
-@Profile("prod") //Roda na produção. Mas o aplication.proprierties deve ser alterado
-//Para funcionar, o seguinte código deve ficar no aplication.properties:  e descomentar o serviceConfig
+//@Profile("prod") //Roda na produção. Mas o aplication.proprierties deve ser alterado
+//Para funcionar deve mudar o aplication.proproperties
 public class NotificadorEmail implements Notificador{
 
-    public NotificadorEmail(){
-        //System.out.println("Notificador Email Real");
-    }
+    @Value("${notificador.email.host-servidor}") //Injetando valor usando uma expression do Spring
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
+
+        System.out.println("Host: "+host+"\nPorta: "+porta);
 
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(),cliente.getEmail(),mensagem);
